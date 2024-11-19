8ccbeb1bbc4b523e52523ac5130cab91511f328e
AcunaTomas <xxtomyacuxx@gmail.com>
2024-11-18 21:45:28 -0300

    Ola

---
+++ Assets/Scripts/CuboBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuboBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        print("Hola! Soy vince el cubo");
+    }
+
+}
